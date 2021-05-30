dc14dc0b4335092233f19da2c1bdf4a6945e21e5
narsimha n <johndoe@example.com>
2021-05-30 08:25:35 -0400

    chapter 11 commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -0,0 +1,23 @@
+package com.bignerdranch.android.criminalintent
+
+import android.content.Context
+import java.lang.IllegalStateException
+
+class CrimeRepository private constructor(context: Context){
+
+    companion object{
+        private var INSTANCE: CrimeRepository? = null
+
+        fun initialize(context: Context){
+            if(INSTANCE == null) {
+                INSTANCE = CrimeRepository(context)
+            }
+        }
+
+        fun get() : CrimeRepository{
+            return INSTANCE ?:
+            throw IllegalStateException("CrimeRepository must be Initialized")
+        }
+    }
+
+}
