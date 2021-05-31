2f1b8bf40487d34ac1ab36291ac012f2a78174af
narsimha n <johndoe@example.com>
2021-05-31 08:25:28 -0400

    chapter 12 commit

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailViewModel.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import java.util.*
@@ -8,7 +9,7 @@ import java.util.*
 class CrimeDetailViewModel():ViewModel() {
     
     private val crimeRepository = CrimeRepository.get()
-    private val crimeIdLiveData = MutableList<UUID>()
+    private val crimeIdLiveData = MutableLiveData<UUID>()
     
     var crimeLiveData: LiveData<Crime?> = 
         Transformations.switchMap(crimeIdLiveData){crimeId -> 
@@ -18,5 +19,9 @@ class CrimeDetailViewModel():ViewModel() {
     fun loadCrime(crimeId: UUID){
         crimeIdLiveData.value = crimeId
     }
-    
+
+    fun saveCrime(crime: Crime){
+//        crimeRepository.updateCrime(crime)
+    }
+
 }
